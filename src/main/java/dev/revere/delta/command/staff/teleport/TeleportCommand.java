503b60c95053d58f05448a9d7f11645228fc38ec
Emmy <lisariesey@gmail.com>
2024-06-27 20:49:31 +0200

    fix: TeleportCommand now allows the sender to teleport another player to themselves rather than just teleporting to a player.

--- src/main/java/dev/revere/delta/command/staff/teleport/TeleportCommand.java
+++ src/main/java/dev/revere/delta/command/staff/teleport/TeleportCommand.java
@@ -9,7 +9,6 @@ import dev.revere.delta.profile.Profile;
 import dev.revere.delta.profile.ProfileService;
 import dev.revere.delta.service.ConfigService;
 import dev.revere.delta.util.CC;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 /**
@@ -25,9 +24,43 @@ public class TeleportCommand extends BaseCommand {
         String[] args = command.getArgs();
 
         if (args.length < 1) {
-            player.sendMessage(CC.translate("&cUsage: /teleport (player)"));
+            player.sendMessage(CC.translate("&cUsage: /teleport (player) [target]"));
             return;
         }
+
+        if (args.length == 2) {
+            Player targetPlayer = player.getServer().getPlayer(args[0]);
+            Player player1 = player.getServer().getPlayer(args[1]);
+
+            if (targetPlayer == null) {
+                player.sendMessage(CC.translate("&fNo player matching &b" + args[0] + " &fis connected to this server."));
+                return;
+            }
+
+            if (player1 == null) {
+                player.sendMessage(CC.translate("&fNo player matching &b" + args[1] + " &fis connected to this server."));
+                return;
+            }
+
+            if (targetPlayer == player1) {
+                player.sendMessage(CC.translate("&cYou cannot teleport a player to themselves."));
+                return;
+            }
+
+            Profile targetProfile = Delta.getInstance().getServiceManager().getService(ProfileService.class).getProfile(targetPlayer.getUniqueId());
+            Profile playerProfile = Delta.getInstance().getServiceManager().getService(ProfileService.class).getProfile(player1.getUniqueId());
+
+            targetPlayer.teleport(player1.getLocation());
+            player.sendMessage(CC.translate(Delta.getInstance().getServiceManager().getService(ConfigService.class).getConfig("messages.yml").getString("staff.teleport.tp-here.sender")
+                    .replace("%rank-color%", String.valueOf(Delta.getInstance().getServiceManager().getService(RankService.class).getHighestRank(targetProfile).getNameColor()))
+                    .replace("%target%", targetPlayer.getDisplayName())));
+            targetPlayer.teleport(player.getLocation());
+            targetPlayer.sendMessage(CC.translate(Delta.getInstance().getServiceManager().getService(ConfigService.class).getConfig("messages.yml").getString("staff.teleport.tp-here.target")
+                    .replace("%rank-color%", String.valueOf(Delta.getInstance().getServiceManager().getService(RankService.class).getHighestRank(playerProfile).getNameColor()))
+                    .replace("%player%", player.getName())));
+            return;
+        }
+
 
         Player targetPlayer = player.getServer().getPlayer(args[0]);
         if (targetPlayer == null) {
