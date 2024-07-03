0993a7f6e568cc3f50ececc6babc4d34d0956ce5
devuxious <remig06@hotmail.com>
2024-07-04 00:56:54 +0200

    feat: added sudo-all command

---
+++ src/main/java/dev/revere/delta/command/staff/SudoAllCommand.java
@@ -0,0 +1,39 @@
+package dev.revere.delta.command.staff;
+
+import dev.revere.delta.Delta;
+import dev.revere.delta.api.command.BaseCommand;
+import dev.revere.delta.api.command.CommandArgs;
+import dev.revere.delta.api.command.annotation.Command;
+import dev.revere.delta.service.ConfigService;
+import dev.revere.delta.util.CC;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+/**
+ * @author Remi
+ * @project Delta
+ * @date 6/17/2024
+ */
+public class SudoAllCommand extends BaseCommand {
+    @Command(name = "sudoall", permission = "delta.staff.sudoall", usage = "/sudoall <command>")
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length < 1) {
+            player.sendMessage(CC.translate("&cUsage: /sudoall <command>"));
+            return;
+        }
+
+        String commandBuilder = String.join(" ", args);
+
+        for (Player target : Delta.getInstance().getServer().getOnlinePlayers()) {
+            target.chat(CC.translate(commandBuilder));
+        }
+
+        player.sendMessage(CC.translate(Delta.getInstance().getServiceManager().getService(ConfigService.class).getConfig("messages.yml").getString("staff.sudo-all"))
+                .replace("%command%", commandBuilder));
+    }
+}
